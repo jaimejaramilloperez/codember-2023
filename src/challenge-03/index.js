f018f350f16e22c12bf23c20bcdcab91ea56d397
Jaime Jaramillo <jaimejaramillo.dev@gmail.com>
2024-10-29 09:33:08 -0500

    feat: Add solution for challenge #3

---
+++ src/challenge-03/index.js
@@ -0,0 +1,45 @@
+import { join } from 'node:path'
+import { readFile } from 'node:fs/promises'
+
+/**
+ * @param {string} passwordList
+ * @param {number} position
+ * @returns {string}
+ */
+function getInvalidKey (passwordList, position = 1) {
+  if (position === 0) throw new RangeError(`Position ${position} out of range`)
+
+  const invalidKeys = []
+
+  for (const password of passwordList.split('\n')) {
+    if (!password) continue
+
+    const [policy, key] = password.split(':')
+    const [min, max, targetChar] = policy.split(/\s|-/)
+
+    let count = 0
+
+    for (const char of key) {
+      if (char === targetChar) count++
+    }
+
+    if (count > max || count < min) {
+      invalidKeys.push(key.trim())
+    }
+  }
+
+  return invalidKeys[position - 1] ?? ''
+}
+
+const FILE_NAME = 'encryption_policies.txt'
+const path = join(import.meta.dirname, FILE_NAME)
+
+const INVALID_PASSWORD_TO_SEARCH = 42
+
+try {
+  const content = await readFile(path, { encoding: 'utf-8' })
+  console.log('submit', getInvalidKey(content, INVALID_PASSWORD_TO_SEARCH))
+} catch (e) {
+  console.log(e.message)
+  process.exit(1)
+}
